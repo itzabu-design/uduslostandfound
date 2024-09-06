54961b87912026b3204d608872bee8f3d4ce6592
itzabu-design <belloumarabubakar274@gmail.com>
2024-09-06 12:17:52 -0700

    Initial commit

--- udus_lost_and_found/urls.py
+++ udus_lost_and_found/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_items, name='templates/view_items'),
+    path('', views.home, name= 'templates/home'),
+    path('', views.sign_up, name='templates/sign_up'),  
     path('report/', views.found_report, name='templates/found_report'),
     path('report/', views.lost_report, name='templates/lost_report'),
+    path('', views.view_items, name='templates/view_items'),
+    path('', views.help_page, name='help_page'),
     path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('admin/manage_users/', views.manage_users, name='manage_users'),
     path('admin/system_settings/', views.system_settings, name='system_settings'),
